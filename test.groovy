84b5e4debaa721225b8fe6854b5156e9a5e0c6b5
Adee <chin92@users.noreply.github.com>
2024-10-04 16:11:20 -0500

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,31 +1,24 @@
-def version1 = '0.7'
-def version2 = '0.0.6'
+
 
 // Function to compare version strings
 boolean isVersionLessThan(String v1, String v2) {
-    def v1Parts = v1.tokenize('.')
-    def v2Parts = v2.tokenize('.')
 
-    def maxLength = Math.max(v1Parts.size(), v2Parts.size())
+    def credentialsId = 'your-credentials-id'
 
-    for (int i = 0; i < maxLength; i++) {
-        def v1Part = (i < v1Parts.size()) ? v1Parts[i].toInteger() : 0
-        def v2Part = (i < v2Parts.size()) ? v2Parts[i].toInteger() : 0
-
-        if (v1Part < v2Part) {
-            return true
-        } else if (v1Part > v2Part) {
-            return false
-        }
+    // Using the SSH key for Git operations
+    withCredentials([sshUserPrivateKey(credentialsId: credentialsId, keyFileVariable: 'SSH_KEY', usernameVariable: 'GIT_USERNAME')]) {
+        // Print to check if the variables are set
+        sh 'echo SSH key path: $SSH_KEY'
+        sh 'echo Git username: $GIT_USERNAME'
+        
+        // Git clone using the SSH key
+        sh '''
+            eval `ssh-agent -s`
+            ssh-add $SSH_KEY
+            git clone git@bitbucket.org:your-repo/project.git
+        '''
     }
-    return false
-}
 
-if (isVersionLessThan(version2, version1)) {
-    def command = 'curl http://example.com'
-    def process = command.execute()
-    process.waitFor()
-    println process.text
-} else {
-    println "${version2} is not less than ${version1}"
+    
+    return false
 }
